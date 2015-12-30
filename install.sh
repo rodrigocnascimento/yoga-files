6d2f9974745ef11461df12232e580deca96732c9
Rodrigo Nascimento <rodrigo.rccn@policiacientifica.sp.gov.br>
2015-12-30 18:11:15 -0200

    Início da instalação do yoga files

--- install.sh
+++ install.sh
@@ -2,40 +2,51 @@
 # YOGA FILES INSTAL
 
 # first we need to check if the directory exists
-#YOGA_HOME=/home/$USER/.yoga-files
 source envvars.sh
-source subroutines/messages.sh
+source ./subroutines/messages.sh
 
 yoga_install(){
-    printf "Instalando YOGA FILES \n"
-    source subroutines/git.sh > /dev/null
-    _IS_GIT_INSTALLED=$?
-    if [ $_IS_GIT_INSTALLED -eq 0 ]; then
-        printf "mkdir ${YOGA_HOME} \n"
-        mkdir ${YOGA_HOME}        
-    fi
+    printf "installing yoga-files \n"
+    source ./subroutines/is_git_installed.#!/bin/sh
+
+    # creating .yoga-files dir
+    mkdir -p ${YOGA_HOME}
+
+    # creating dir to backup old files
+    mkdir -p ${YOGA_HOME}/old_files
+
+    # backing up old files
+    mv ~/.bashrc ${YOGA_HOME}/old_files
+
+    #if [ $_IS_GIT_INSTALLED -eq 0 ]; then
+    #    printf "cloning yoga-files \n"
+    #    git clone https://github.com/rodreego/yoga-files.git ${YOGA_HOME}
+    #fi
 }
 
 yoga_quit(){
-    echo "Saindo"
+    yoga_fail
 }
 
-#if [ "$(id -u)" != "0" ]; then
-#   echo "This script must be run as root" 1>&2
-#   exit 1
-#fi
+is_yoga_installed?(){
+    if [ ! -d $YOGA_HOME ]; then
+        echo -n "yoga-files isn't installed ~> wish to proceed the installation? [Y/N] "
+        read  _ANSWER
 
-if [ ! -d $YOGA_HOME ]; then
-    echo -n "Yoga Files não está instalado ~> Deseja instalar? [S/N] "
-    read  _ANSWER
-
-    if [[ "$_ANSWER" =~ [Ss] ]]
-     then
-        yoga_install
-    elif [[ "$_ANSWER" =~ [Nn] ]]
-     then
-        yoga_quit
+        if [[ "$_ANSWER" =~ [Yy] ]]
+         then
+            yoga_install
+        elif [[ "$_ANSWER" =~ [Nn] ]]
+         then
+            yoga_quit
+        else
+            echo "answer YES (Yy) or NO (Nn)"
+            is_yoga_installed?
+        fi
     else
-        echo "Responda SIM OU NÃO" 
+        echo "yoga-files is installed, wish update?"
     fi
-fi
+}
+
+# Function that verifies if yoga-files is installed
+is_yoga_installed?
