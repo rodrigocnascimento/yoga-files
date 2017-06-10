f72fbe6b92c19322373215c96e112616d579801f
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2017-06-10 08:10:10 -0300

    removing PS1 configs

--- workspace.sh
+++ workspace.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-PROMPT_COMMAND=prompt
 
- # Carrega os arquivos de ambiente
+# Carrega os arquivos de ambiente
 source ~/.yoga/.aliases
 source ~/.yoga/.functions
 source ~/.yoga/.envvars
-source ~/.yoga/.bash_powerline.sh
 
 if [ -f ~/.profile_custom ]; then
     source ~/.profile_custom
