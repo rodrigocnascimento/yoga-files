59caf88f6fcc13566b6f8ad8e14a5234af7a2949
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2017-07-11 19:58:16 -0300

    _|_

--- install.sh
+++ install.sh
@@ -4,65 +4,65 @@ source envvars.sh
 source messages.sh
 
 yoga_install(){
-	if [[ "$OSTYPE" == "darwin"* ]]; then
-		cp workspace.sh $YOGA_HOME/.workspace
-		cp files/home_aliases $YOGA_HOME/.aliases
-		cp files/ps1 $YOGA_HOME/.ps1
-		cp files/home_functions $YOGA_HOME/.functions
-		cp files/home_envvars $YOGA_HOME/.envvars
-		cp files/.gitconfig ~/.gitconfig
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    cp workspace.sh $YOGA_HOME/.workspace
+    cp files/home_aliases $YOGA_HOME/.aliases
+    cp files/ps1 $YOGA_HOME/.ps1
+    cp files/home_functions $YOGA_HOME/.functions
+    cp files/home_envvars $YOGA_HOME/.envvars
+    cp files/.gitconfig ~/.gitconfig
 
-		# echo -e "\n source " $YOGA_HOME/.workspace "\n" >> ~/.profile
-		source_workspace="\n source " $YOGA_HOME/.workspace "\n"
-		# sed -e "\|$source_workspace|h; \${x;s|$source_workspace||;{g;t};a\\" -e "$source_workspace" -e "}" ~/.profile
-		grep -q -F $source_workspace ~/.profile || echo $source_workspace >> ~/.profile
-		source ~/.profile
+    # echo -e "\n source " $YOGA_HOME/.workspace "\n" >> ~/.profile
+    source_workspace="source $YOGA_HOME/.workspace"
+    # sed -e "\|$source_workspace|h; \${x;s|$source_workspace||;{g;t};a\\" -e "$source_workspace" -e "}" ~/.profile
+    # grep -q -F $source_workspace ~/.profile || echo $source_workspace >> ~/.profile
+    ack $source_workspace ~/.profile || echo $source_workspace >> ~/.profile
 
-		yoga_ok
-	fi
+    yoga_ok
+  fi
 }
 
 yoga_update(){
-	echo "ROUND 2 ... FIGHT! Rebasing REPO"
-	git pull --rebase
+  echo "ROUND 2 ... FIGHT! Rebasing REPO"
+  git pull --rebase
 
-	echo "Reinstalling"
-	yoga_install
+  echo "Reinstalling"
+  yoga_install
 
-	echo "I think we're done"
+  echo "I think we're done"
 }
 
 yoga_quit(){
-	yoga_fail
+  yoga_fail
 }
 
 is_yoga_installed?(){
 if [ ! -d $YOGA_HOME ]; then
-	echo -n ".yoga não está instalado ~> INSTALL [Y/N] "
-	read  _ANSWER
+  echo -n ".yoga não está instalado ~> INSTALL [Y/N] "
+  read  _ANSWER
 
-	if [[ "$_ANSWER" =~ [Yy] ]]
-	then
-		printf "Installing ~/.yoga \n"
-		mkdir $YOGA_HOME
+  if [[ "$_ANSWER" =~ [Yy] ]]
+  then
+    printf "Installing ~/.yoga \n"
+    mkdir $YOGA_HOME
 
-		echo "ROUND 1 files!"
-		yoga_install
-	elif [[ "$_ANSWER" =~ [Nn] ]]
-	then
-		yoga_quit
-	else
-		echo "YES (Yy) NO (Nn) pls :)"
-		is_yoga_installed?
-	fi
+    echo "ROUND 1 files!"
+    yoga_install
+  elif [[ "$_ANSWER" =~ [Nn] ]]
+  then
+    yoga_quit
+  else
+    echo "YES (Yy) NO (Nn) pls :)"
+    is_yoga_installed?
+  fi
 else
-	echo ".yoga está instalado ~> UPDATE [Y/N] "
-	read  _ANSWER
+  echo ".yoga está instalado ~> UPDATE [Y/N] "
+  read  _ANSWER
 
-	if [[ "$_ANSWER" =~ [Yy] ]]
-	then
-		yoga_update
-	fi
+  if [[ "$_ANSWER" =~ [Yy] ]]
+  then
+    yoga_update
+  fi
 fi
 }
 
