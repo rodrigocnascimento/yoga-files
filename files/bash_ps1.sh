8854d85cc17b9d22b53f54c7c2658359506a4514
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2017-07-24 12:55:30 -0300

    touched...

--- files/bash_ps1.sh
+++ files/bash_ps1.sh
@@ -8,54 +8,56 @@ COLRED="\[\033[31m\]"
 COLCLEAR="\[\033[0m\]"
 
 function we_are_in_git_work_tree {
-	git rev-parse --is-inside-work-tree &> /dev/null
+  git rev-parse --is-inside-work-tree &> /dev/null
 }
 
 function parse_git_branch {
-	if we_are_in_git_work_tree
-	then
-		local BR=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD 2> /dev/null)
-		local COUNT_MODIFIED=$(git status -s | wc -l | sed 's/ //g')
-		local COUNT_AHEAD=0
+  if we_are_in_git_work_tree
+  then
+    local BR=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD 2> /dev/null)
+    local COUNT_MODIFIED=$(git status -s | wc -l | sed 's/ //g')
+    local COUNT_AHEAD=0
 
-		if [ $COUNT_MODIFIED -gt "0" ]
-		then
-			let ORIGIN_EXIST=$(git branch -a | ack remotes/origin/$BR | wc -l)
-			if [ $ORIGIN_EXIST -ge "1" ]
-			then
-				COUNT_AHEAD=$(git log origin/$BR..$BR --oneline | wc -l | sed 's/ //g')
-			fi
-		fi
+    if [ $COUNT_MODIFIED -gt "0" ]
+    then
+      let ORIGIN_EXIST=$(git branch -a | ack remotes/origin/$BR | wc -l)
+      if [ $ORIGIN_EXIST -ge "1" ]
+      then
+        COUNT_AHEAD=$(git log origin/$BR..$BR --oneline | wc -l | sed 's/ //g')
+      else
+        COUNT_AHEAD=$(git log $BR..$BR --oneline | wc -l | sed 's/ //g')
+      fi
+    fi
 
-		if [ "$BR" == HEAD ]
-		then
-			local NM=$(git name-rev --name-only HEAD 2> /dev/null)
-			if [ "$NM" != undefined ]
-			then echo -n "@$NM"
-			else git rev-parse --short HEAD 2> /dev/null
-			fi
-		else
-			echo -n "$BR ≠ $COUNT_MODIFIED ⇡ $COUNT_AHEAD"
-		fi
-	fi
+    if [ "$BR" == HEAD ]
+    then
+      local NM=$(git name-rev --name-only HEAD 2> /dev/null)
+      if [ "$NM" != undefined ]
+      then echo -n "@$NM"
+      else git rev-parse --short HEAD 2> /dev/null
+      fi
+    else
+      echo -n "$BR ≠ $COUNT_MODIFIED ⇡ $COUNT_AHEAD"
+    fi
+  fi
 }
 
 function parse_git_status {
-	if we_are_in_git_work_tree
-	then
-		local ST=$(git status --short 2> /dev/null)
-		if [ -n "$ST" ]
-		then echo -n " ? "
-		else echo -n " o "
-		fi
-	fi
+  if we_are_in_git_work_tree
+  then
+    local ST=$(git status --short 2> /dev/null)
+    if [ -n "$ST" ]
+    then echo -n " ? "
+    else echo -n " o "
+    fi
+  fi
 }
 
 function pwd_depth_limit_2 {
-	if [ "$PWD" = "$HOME" ]
-	then echo -n "~"
-	else pwd | sed -e "s|.*/\(.*/.*\)|\1|"
-	fi
+  if [ "$PWD" = "$HOME" ]
+  then echo -n "~"
+  else pwd | sed -e "s|.*/\(.*/.*\)|\1|"
+  fi
 }
 
 
