f62a2648ae92453599dae45e1db9bc2cac9036b3
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2017-10-31 12:02:47 -0200

    Good eyes

--- files/bash_ps1.sh
+++ files/bash_ps1.sh
@@ -60,5 +60,5 @@ function pwd_depth_limit_2 {
 
 # export all these for subshells
 export -f parse_git_branch parse_git_status we_are_in_git_work_tree pwd_depth_limit_2
-export PS1="$COLBLUE\$(pwd_depth_limit_2)\$(parse_git_status)$COLGRAY\$(parse_git_branch)$COLYELLOW\n "
+export PS1="$COLBLUE\$(pwd_depth_limit_2)\$(parse_git_status)$COLGRAY\$(parse_git_branch)\n 👁 $COLYELLOW" 
 export TERM="xterm-256color"
