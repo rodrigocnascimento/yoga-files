44066e3079e4caca0ff5e39a8f6ce07a51a45700
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2019-08-16 15:27:42 -0300

    removing specific aliases

--- files/aliases.sh
+++ files/aliases.sh
@@ -6,9 +6,3 @@ alias ~='cd ~'
 
 # Ls Aliases
 alias lsd='ls -lahF ${colorflag}'
-
-# Visual Studio Code
-alias code='/Applications/Visual\ Studio\ Code.app/Contents/MacOS/Electron'
-
-# Google Chrome Canary
-alias canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
