b980c4c7090f0fa01bff900d45eaffb5faae5545
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2019-08-16 15:27:53 -0300

    adding great functions

--- files/functions.sh
+++ files/functions.sh
@@ -1,6 +1,3 @@
-function canary_debugger() {
-  canary --remote-debugging-port=$1 http://localhost:$1
-}
 
 function pid_port() {
   echo $(lsof -n -i :$1 | grep LISTEN | awk '{print $2}')
@@ -10,3 +7,10 @@ function kill_port() {
   kill -9 $(pid_port $1)
 }
 
+function take() {
+  mkdir -p $@ && cd ${@:$#}
+}
+
+function ssh_agent_run() {
+  eval `ssh-agent -s`; ssh-add; ssh-add -k ~/.ssh/$@
+}
