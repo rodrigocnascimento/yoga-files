8c8d838d5c048b4805d23bf5a7b973ad4ff2cf6e
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2020-07-16 15:24:32 -0300

    removing bash_ps1 in favor of powerlevel10k

--- loader.sh
+++ loader.sh
@@ -3,7 +3,6 @@
 # Carrega os arquivos de ambiente
 source ~/.yoga/.aliases
 source ~/.yoga/.functions
-source ~/.yoga/.bash_ps1
 
 # Load custom aliases
 if [ -f ~/.custom.aliases.sh ]; then
@@ -14,5 +13,3 @@ fi
 if [ -f ~/.custom.functions.sh ]; then
   source ~/.custom.functions.sh
 fi
-
-ssh_agent_run rodrigo.nascimento
