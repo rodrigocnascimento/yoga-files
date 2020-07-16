7af3ef5598c3cda2a5417ead98e659a4dfed6358
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2020-07-16 15:22:20 -0300

    Adding echo_ip alias

--- files/aliases.sh
+++ files/aliases.sh
@@ -1,10 +1,5 @@
-# cd back
 alias ..='cd ..'
-
-# Home
 alias ~='cd ~'
-
-# Ls Aliases
 alias lsd='ls -lahF ${colorflag}'
-
 alias please='sudo $(fc -ln -1)'
+alias echo_ip='curl http://ipecho.net/plain; echo'
