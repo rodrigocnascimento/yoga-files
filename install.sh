3ce6fbdb742d72686fc3a1f40e671e71e921c521
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2020-07-20 15:34:32 -0300

    texto de impacto

--- install.sh
+++ install.sh
@@ -10,16 +10,16 @@ source lib/*
 
 function yoga_install {
   if does_yoga_need_update; then
-    echo "Rebasing REPO git pull --rebase" && 
+    echo "Rebasing REPO git pull --rebase"
     
-    # git pull --rebase && 
-    
-    source_scripts &&
-
-    yoga_success "ROUND 2 ... FIGHT!" 
-    
-    return 0
+    # git pull --rebase
   fi
+
+  source_scripts &&
+
+  yoga_success "ROUND 2 ... FIGHT!" 
+
+  return 0
 }
 
 function yoga {
