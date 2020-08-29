8aeb0e2e8290d0a734f9ef23d0a39c76a31169d3
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2020-08-29 17:41:26 -0300

    Changed the filename

---
+++ yoga.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+set -e
+
+YOGA_HOME=~/.yoga
+
+export YOGA_HOME
+
+source lib/*
+
+function yoga_install() {
+  yoga_updater &&
+    install_system_scripts &&
+    install_git_scripts &&
+    yoga_success "ROUND 2 ... FIGHT!"
+
+  return 0
+}
+
+function yoga() {
+  (
+    yoga_install
+  ) || (
+    yoga_success "yoga was installed"
+  )
+}
+
+yoga
