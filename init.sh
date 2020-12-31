3a294a7ea7cb018db4a83a32ee2e5cd4ca50235e
Rodrigo Nascimento <rodrigo.nascimento@mobly.com.br>
2020-12-31 08:55:52 -0300

    tentando colocar o vimrc para dentro do projeto

--- init.sh
+++ init.sh
@@ -22,6 +22,9 @@ ssh_agent_run "bitbucket"
 
 export FZF_DEFAULT_OPTS='--height 40% --border --pointer=👉'
 
+export MYVIMRC=$YOGA_HOME/core/terminal/vimrc
+export VIMINIT='source $MYVIMRC'
+
 # check for updates
 if [[ -d "$YOGA_HOME" ]]; then
   yoga_warn "ROUND 2 ... UPDATING!" && update_yoga
