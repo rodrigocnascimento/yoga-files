8319926aee84d45cfdbed7c41769c6ec6fa9be93
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2022-04-13 09:25:38 -0300

    Update install.sh

--- core/git/install.sh
+++ core/git/install.sh
@@ -4,8 +4,11 @@ DIR="$(dirname "$(readlink -f "$0")")"
 
 source "$DIR/../utils.sh"
 
-yoga_action "copying" "core/git/.gitconfig to ~"
+yoga_action "copying default .gitconfig" "core/git/.gitconfig to ~"
 cp core/git/.gitconfig ~
+
+yoga_action "copying .gitconfig_basic" "core/git/.gitconfig_basic to ~"
+cp core/git/.gitconfig_basic ~/code/.gitconfig
 
 yoga_action "copying" "core/git/.gitignore_global to ~"
 cp core/git/.gitignore_global ~
