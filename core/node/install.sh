15e8e299d520738778f3da8e2eb48bdc678f483c
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2022-04-13 08:56:17 -0300

    Update install.sh

--- core/node/install.sh
+++ core/node/install.sh
@@ -25,15 +25,6 @@ else
     yoga_success "lts node version installed"
 fi
 
-# @vue/cli
-if which vue &> /dev/null; then
-    yoga_success "vue already installed"
-else
-    yoga_action "install" "npm install -g @vue/cli"
-    npm install -g @vue/cli
-    yoga_success "@vue/cli installed"
-fi
-
 # eslint
 if which eslint &> /dev/null; then
     yoga_success "eslint already installed"
@@ -43,32 +34,6 @@ else
     yoga_success "eslint installed"
 fi
 
-# httpster
-if which httpster &> /dev/null; then
-    yoga_success "httpster already installed"
-else
-    yoga_action "install" "npm install -g httpster"
-    npm install -g httpster
-    yoga_success "httpster installed"
-fi
-
-# serverless
-if which serverless &> /dev/null; then
-    yoga_success "serverless already installed"
-else
-    yoga_action "install" "npm install -g serverless"
-    npm install -g serverless
-    yoga_success "serverless installed"
-fi
-
-# jsinspect
-if which jsinspect &> /dev/null; then
-    yoga_success "jsinspect already installed"
-else
-    yoga_action "install" "npm install -g jsinspect"
-    npm install -g jsinspect
-    yoga_success "jsinspect installed"
-fi
 
 # npkill
 if which npkill &> /dev/null; then
